e679e0636c08b6eea705e24edf9443c39239b39a
Iruna11 <147715301+Iruna11@users.noreply.github.com>
2025-05-30 02:04:58 +0300

    added calendar, form, schedule and sidebar components

--- eleksinternshipproj.client/src/app/app.module.ts
+++ eleksinternshipproj.client/src/app/app.module.ts
@@ -1,19 +1,42 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { CalendarComponent } from './shared/components/schedule/calendar/calendar.component';
+import { CalendarFormComponent } from './shared/components/schedule/calendar-form/calendar-form.component';
+import { ScheduleComponent } from './shared/components/schedule/schedule/schedule.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+
+// Angular Material
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    CalendarFormComponent,
+    ScheduleComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    // Material modules
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule 
+],
   providers: [
     provideAnimationsAsync()
   ],
