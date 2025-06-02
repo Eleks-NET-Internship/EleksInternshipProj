c6799d98c0c0959ce3ddab6a5edf56e00e53629a
Nazarii Liushniak <nazarii.liushniak.pz.2022@lpnu.ua>
2025-06-02 13:48:56 +0300

    NAV-8-added auto login after registration

--- eleksinternshipproj.client/src/app/core/services/auth/auth.service.ts
+++ eleksinternshipproj.client/src/app/core/services/auth/auth.service.ts
@@ -12,9 +12,9 @@ export class AuthService {
   constructor(private readonly http: HttpClient) { }
 
   login(credentials: { email: string; password: string }) {
-    this.http.post<{ token: string }>('http://localhost:5142/api/auth/login', credentials)
+    this.http.post<{ accessToken: string }>('http://localhost:5142/api/auth/login', credentials)
       .subscribe(response => {
-        sessionStorage.setItem(this.TOKEN_KEY, response.token);
+        sessionStorage.setItem(this.TOKEN_KEY, response.accessToken);
       });
   }
 
@@ -26,8 +26,13 @@ export class AuthService {
     this.http.post('http://localhost:5142/api/auth/register', registerPayload, { observe: 'response' })
       .subscribe(response => {
         if (response.ok) {
-          console.log("Successful register.")
+          console.log("Successful register.");
+          this.login({ email: registerPayload.email, password: registerPayload.password });
         }
       });
+  }
+
+  loginWithGoogle() {
+    //this.http.post('https://localhost:5142/api/auth/login/google');
   }
 }
