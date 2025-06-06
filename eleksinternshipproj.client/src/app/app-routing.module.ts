3a788cc978c16c74ccf66856d154d9e3660e0334
NazarSlobodian <nazar.slobodian.pz.2022@lpnu.ua>
2025-06-06 20:08:40 +0300

    NAV-32: Added guard. Previous commits were meant to be NAV-32

--- eleksinternshipproj.client/src/app/app-routing.module.ts
+++ eleksinternshipproj.client/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './feature/login/components/login/login.component';
 import { RegisterComponent } from './feature/register/components/register/register.component';
 import { WeatherForecastComponent } from './feature/weatherForecast/components/weather-forecast/weather-forecast.component';
+import { authGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'forecast', component: WeatherForecastComponent },
-  { path: '', component: WeatherForecastComponent },
+  { path: 'forecast', component: WeatherForecastComponent, canActivate: [authGuard] }, // when Home component arrives, switch to it here
+  { path: '', component: WeatherForecastComponent, canActivate: [authGuard] }, // and here
   { path: '**', redirectTo: '/' }
 ];
 
