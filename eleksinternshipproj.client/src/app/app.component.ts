481827c7119fa69f93eed2828cc6f40353b6053f
NazarSlobodian <nazar.slobodian.pz.2022@lpnu.ua>
2025-06-06 20:58:37 +0300

    NAV-33: Forgot to delete redundant code

--- eleksinternshipproj.client/src/app/app.component.ts
+++ eleksinternshipproj.client/src/app/app.component.ts
@@ -1,31 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from './core/services/auth/auth.service';
-import { NavigationEnd, Router } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-
-  constructor(private auth: AuthService, private router: Router) {
-    // ugly not secure, but should work
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        const url = new URL(window.location.href);
-        const token = url.searchParams.get('accessToken');
-
-        if (token) {
-          this.auth.setToken(token);
-
-          const cleanedUrl = url.origin + url.pathname + url.hash;
-          window.history.replaceState({}, '', cleanedUrl);
-        }
-      }
-    })
-  }
 
   ngOnInit(): void {
     //
