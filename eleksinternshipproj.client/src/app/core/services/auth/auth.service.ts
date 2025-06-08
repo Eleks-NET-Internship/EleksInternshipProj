bef1cf91e6284ef8017de1600047060bdc857dda
NazarSlobodian <126600781+NazarSlobodian@users.noreply.github.com>
2025-06-08 13:17:14 +0300

    NAV-32: Redirection (#18)
    * NAV-33: Redirect to home after normal login
    
    * NAV-33: Added redirect for Google Auth, moved error handling from service, made name/surname optional on registration
    
    * NAV-32: Added guard. Previous commits were meant to be NAV-32
    
    * NAV-32: Changed redirection flow, added navigate to '/login' on logout
    
    * NAV-33: Forgot to delete redundant code

--- eleksinternshipproj.client/src/app/core/services/auth/auth.service.ts
+++ eleksinternshipproj.client/src/app/core/services/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,7 @@ export class AuthService {
   private readonly apiBaseUrl = 'https://localhost:7050';
   private readonly TOKEN_KEY = 'access_token';
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient, private router: Router) { }
 
   getToken() {
     return sessionStorage.getItem(this.TOKEN_KEY);
@@ -18,28 +20,20 @@ export class AuthService {
     sessionStorage.setItem(this.TOKEN_KEY, token);
   }
 
-  login(credentials: { email: string; password: string }) {
-    this.http.post<{ accessToken: string }>(this.apiBaseUrl + '/api/auth/login', credentials)
-      .subscribe(response => {
-        this.setToken(response.accessToken);
-      });
+  login(credentials: { email: string; password: string }): Observable<{ accessToken: string }> {
+    return this.http.post<{ accessToken: string }>(`${this.apiBaseUrl}/api/auth/login`, credentials);
   }
 
   logout() {
     sessionStorage.removeItem(this.TOKEN_KEY);
+    this.router.navigate(['/login']);
   }
 
   register(registerPayload: { firstName: string, lastName: string, username: string, email: string, password: string }) {
-    this.http.post(this.apiBaseUrl + '/api/auth/register', registerPayload, { observe: 'response' })
-      .subscribe(response => {
-        if (response.ok) {
-          console.log("Successful register.");
-          this.login({ email: registerPayload.email, password: registerPayload.password });
-        }
-      });
+    return this.http.post(`${this.apiBaseUrl}/api/auth/register`, registerPayload);
   }
 
   loginWithGoogle() {
-    window.location.href = this.apiBaseUrl + '/api/auth/login/google?returnUrl=https://localhost:4200/home';
+    window.location.href = `${this.apiBaseUrl}/api/auth/login/google?returnUrl=/forecast`;
   }
 }
