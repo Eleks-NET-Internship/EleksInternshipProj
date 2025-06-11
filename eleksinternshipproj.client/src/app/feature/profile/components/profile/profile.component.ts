793e7560893fb565f5e618bcbc9c1b4f7555bc84
NazarSlobodian <126600781+NazarSlobodian@users.noreply.github.com>
2025-06-11 19:50:31 +0300

    NAV-44: Profile UI (#28)
    * NAV-44: Deleted weather forecast, added profile component
    
    * NAV-49: Sidebar switch case test
    
    * NAV-49: Sidebar redirect added. However, it gets reset after every redirect
    
    * NAV-49: Sidebar redirect works. Icons not centered
    
    * NAV-49: Icons recentered
    
    * NAV-44: Profile data received
    
    * NAV-44: Profile data sent, typo fixed
    
    * NAV-44: Huge css fix
    
    * NAV-44: Again?

--- eleksinternshipproj.client/src/app/feature/profile/components/profile/profile.component.ts
+++ eleksinternshipproj.client/src/app/feature/profile/components/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ProfileService } from '../../services/profile.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProfileDto, ProfileResponse, UpdateProfileDto } from '../../models/profile-models';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +10,60 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  profileForm: FormGroup;
 
+  email: string = "";
+
+  constructor(private fb: FormBuilder, private profileService: ProfileService, private snackBar: MatSnackBar) {
+    this.profileForm = this.fb.group({
+      username: ["", Validators.required],
+      firstname: [""],
+      lastname: [""]
+    });}
+
+  ngOnInit(): void {
+    this.getProfile();
+  }
+
+  getProfile(): void {
+    this.profileService.getProfile().subscribe({
+      next: (response: ProfileResponse) => {
+
+        this.profileForm.patchValue({
+          username: response.data.username,
+          firstname: response.data.firstName,
+          lastname: response.data.lastName
+        });
+        this.email = response.data.email;
+      },
+      error: (error) => {
+        console.error(error.error.message);
+      }
+    });
+  }
+
+  onSave(): void {
+    const formValues = this.profileForm.value;
+
+    const userProfile: UpdateProfileDto = {
+      username: formValues.username.trim(),
+      firstName: formValues.firstname?.trim() || undefined,
+      lastName: formValues.lastname?.trim() || undefined,
+    };
+
+    this.profileService.updateProfile(userProfile).subscribe({
+      next: (response) => {
+        this.snackBar.open('Дані оновлено!', 'Закрити', {
+          duration: 5000,
+        });
+      },
+      error: (error) => {
+        console.error(error.error.message);
+        this.snackBar.open('Не вдалось оновити дані :(', 'Закрити', {
+          duration: 5000,
+          panelClass: ['snackbar-error']
+        });
+      }
+    });
+  }
 }
