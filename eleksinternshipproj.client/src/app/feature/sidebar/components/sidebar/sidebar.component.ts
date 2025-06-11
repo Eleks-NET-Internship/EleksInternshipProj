9185213a76a81373230901a5ac654919c675d937
NazarSlobodian <nazar.slobodian.pz.2022@lpnu.ua>
2025-06-11 12:37:10 +0300

    NAV-49: Sidebar switch case test

--- eleksinternshipproj.client/src/app/feature/sidebar/components/sidebar/sidebar.component.ts
+++ eleksinternshipproj.client/src/app/feature/sidebar/components/sidebar/sidebar.component.ts
@@ -18,9 +18,56 @@ export class SidebarComponent {
     { icon: 'person', label: 'Профіль' },
     { icon: 'settings', label: 'Налаштування' }
   ];
-  
+
   onMenuClick(item: any) {
     console.log('Clicked:', item.label);
+    switch (item.label) {
+
+      case 'Простір': {
+        console.log('Простір');
+        break;
+      }
+
+      case 'Календар': {
+        console.log('Календар');
+        break;
+      }
+
+      case 'Розклад': {
+        console.log('Розклад');
+        break;
+      }
+
+      case 'Завдання': {
+        console.log('Завдання');
+        break;
+      }
+
+      case 'Нотатки': {
+        console.log('Нотатки');
+        break;
+      }
+
+      case 'Події': {
+        console.log('Події');
+        break;
+      }
+
+      case 'Статистика': {
+        console.log('Статистика');
+        break;
+      }
+
+      case 'Профіль': {
+        console.log('Профіль');
+        break;
+      }
+
+      case 'Налаштування': {
+        console.log('Налаштування');
+        break;
+      }
+    }
   }
-  
+
 }
