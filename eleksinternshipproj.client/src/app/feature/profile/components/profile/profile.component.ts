833ff2b25f697a616e4437b58f7fc70afe408bcf
NazarSlobodian <nazar.slobodian.pz.2022@lpnu.ua>
2025-06-11 18:09:10 +0300

    NAV-44: Profile data received

--- eleksinternshipproj.client/src/app/feature/profile/components/profile/profile.component.ts
+++ eleksinternshipproj.client/src/app/feature/profile/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ProfileService } from '../../services/profile.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProfileDto, ProfileResponse } from '../../models/profile-models';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  profileForm: FormGroup;
 
+  email: string = "";
+
+  constructor(private fb: FormBuilder, private profileService: ProfileService) {
+    this.profileForm = this.fb.group({
+      username: ["", Validators.required],
+      firstname: [""],
+      lastname: [""]
+    });}
+
+  ngOnInit(): void {
+    this.getProfile();
+  }
+
+  getProfile(): void {
+    this.profileService.getProfile().subscribe({
+      next: (response: ProfileResponse) => {
+
+        this.profileForm.patchValue({
+          username: response.data.username,
+          firstname: response.data.firstName,
+          lastname: response.data.lastName
+        });
+        this.email = response.data.email;
+      },
+      error: (error) => {
+        console.error(error.error.message);
+      }
+    });
+  }
+
+  onSave(): void {
+    // do nothing at all
+  }
 }
---
+++ eleksinternshipproj.client/src/app/feature/profile/services/profile.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProfileDto, ProfileResponse, UpdateProfileDto } from '../models/profile-models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  private readonly apiBaseUrl = 'https://localhost:7050';
+  private readonly TOKEN_KEY = 'access_token';
+
+  constructor(private readonly http: HttpClient) { }
+
+  updateProfile(newData: UpdateProfileDto) {
+    return this.http.patch<UpdateProfileDto>(`${this.apiBaseUrl}/api/profile`, newData);
+  }
+
+  getProfile(): Observable<ProfileResponse> {
+    return this.http.get<ProfileResponse>(`${this.apiBaseUrl}/api/profile`);
+  }
+}
