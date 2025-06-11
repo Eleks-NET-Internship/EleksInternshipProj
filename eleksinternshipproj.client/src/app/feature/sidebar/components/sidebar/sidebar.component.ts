4801ee2f562337647a2b60b9ddb5d56dd7e515c1
NazarSlobodian <126600781+NazarSlobodian@users.noreply.github.com>
2025-06-11 17:46:44 +0300

    NAV-49: Sidebar half-functional (#26)
    * NAV-44: Deleted weather forecast, added profile component
    
    * NAV-49: Sidebar switch case test
    
    * NAV-49: Sidebar redirect added. However, it gets reset after every redirect
    
    * NAV-49: Sidebar redirect works. Icons not centered
    
    * NAV-49: Icons recentered

--- eleksinternshipproj.client/src/app/feature/sidebar/components/sidebar/sidebar.component.ts
+++ eleksinternshipproj.client/src/app/feature/sidebar/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,17 +11,80 @@ export class SidebarComponent {
   sidenavItems = [
     { icon: 'workspace', label: 'Простір' },
     { icon: 'calendar_today', label: 'Календар' },
-    { icon: 'schedule', label: 'Розклад', active: true },
+    { icon: 'schedule', label: 'Розклад' },
     { icon: 'assignment', label: 'Завдання' },
     { icon: 'note', label: 'Нотатки' },
     { icon: 'event', label: 'Події' },
     { icon: 'bar_chart', label: 'Статистика' },
-    { icon: 'person', label: 'Профіль' },
-    { icon: 'settings', label: 'Налаштування' }
+    { icon: 'person', label: 'Профіль', active: true },
+    { icon: 'settings', label: 'Налаштування' },
   ];
-  
-  onMenuClick(item: any) {
-    console.log('Clicked:', item.label);
+
+  constructor(private router: Router) { }
+
+  onMenuClick(clickedItem: any) {
+
+    this.sidenavItems = this.sidenavItems.map(item => ({
+      ...item,
+      active: item.label === clickedItem.label
+    }));
+
+    switch (clickedItem.label) {
+
+      case 'Простір': {
+        console.log('Простір');
+        alert("Not implemented :("); // no component for space selection
+        break;
+      }
+
+      case 'Календар': {
+        console.log('Календар');
+        this.router.navigate(['/calendar']);
+        break;
+      }
+
+      case 'Розклад': {
+        console.log('Розклад');
+        this.router.navigate(['/schedule']);
+        break;
+      }
+
+      case 'Завдання': {
+        console.log('Завдання'); // same
+        alert("Not implemented :(");
+        break;
+      }
+
+      case 'Нотатки': {
+        console.log('Нотатки'); // same
+        alert("Not implemented :(");
+        break;
+      }
+
+      case 'Події': {
+        console.log('Події'); // same
+        alert("Not implemented :(");
+        break;
+      }
+
+      case 'Статистика': {
+        console.log('Статистика'); // same
+        alert("Not implemented :(");
+        break;
+      }
+
+      case 'Профіль': {
+        console.log('Профіль');
+        this.router.navigate(['/profile']);
+        break;
+      }
+
+      case 'Налаштування': {
+        console.log('Налаштування'); // same
+        alert("Not implemented :(");
+        break;
+      }
+    }
   }
-  
+
 }
