fe149d0eb8be5e138c3fded15417fe0c297869d1
MykhailivVolodymyr <volodymyrmykhailiv2005@gmail.com>
2025-06-11 23:32:01 +0300

    Dto and service for Event Ui added

--- eleksinternshipproj.client/src/app/feature/events/components/events/events.component.ts
+++ eleksinternshipproj.client/src/app/feature/events/components/events/events.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { EventsService } from '../../services/events.service';
+import { EventDto } from '../../models/events-models';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-events',
@@ -9,4 +11,15 @@ import { EventsService } from '../../services/events.service';
 export class EventsComponent {
   constructor(private eventsService: EventsService) { }
 
+    ngOnInit(): void {
+     // const spaceId = this.spaceService.getSpaceId();     Тут буде витягнення spaceId з сервісу, коли він буде реалізований, поки що дефолтне значення 1
+    const spaceId = 1;  
+    this.eventsService.getAll(spaceId)
+      .pipe(
+        map((response: { data: EventDto[] }) => response.data)
+      )
+      .subscribe(events => {
+        console.log(events); 
+      });
+  }
 }
