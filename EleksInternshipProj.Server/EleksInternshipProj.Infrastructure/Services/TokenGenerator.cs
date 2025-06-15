89dc3da512b669f573d64d6b91830776682e3b17
NazarSlobodian <nazar.slobodian.pz.2022@lpnu.ua>
2025-06-15 16:36:23 +0300

    NAV-51: Another namespace fix

--- EleksInternshipProj.Server/EleksInternshipProj.Infrastructure/Services/TokenGenerator.cs
+++ EleksInternshipProj.Server/EleksInternshipProj.Infrastructure/Services/TokenGenerator.cs
@@ -1,10 +1,12 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using EleksInternshipProj.Application.Services;
-using EleksInternshipProj.Infrastructure.Extensions;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+
+using EleksInternshipProj.Application.Services;
+using EleksInternshipProj.Infrastructure.Extensions;
 
 namespace EleksInternshipProj.Infrastructure.Services
 {
