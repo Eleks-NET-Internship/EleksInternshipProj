f71ccdceafa465d7a405446043aab2991bee46fe
Nazarii Liushniak <nazarii.liushniak.pz.2022@lpnu.ua>
2025-06-16 17:09:37 +0300

    NAV-46-implemented calendar form functional

--- eleksinternshipproj.client/src/app/feature/calendar/models/calendar-models.ts
+++ eleksinternshipproj.client/src/app/feature/calendar/models/calendar-models.ts
@@ -1,5 +1,26 @@
-export interface CalendarsDTO {
-  mock: string
+export interface TaskDTO {
+  id: number,
+  eventId: number,
+  statusId: number,
+  statusName: string,
+  name: string,
+  eventTime: string,
+  isDeadline: boolean,
+  description: string
+}
 
-  mock2?: string
+export interface UniqueEventDTO {
+  id: number,
+  eventId: number,
+  eventName: string,
+  eventTime: string,
+  spaceId: number,
+  markers: MarkerDto[]
+}
+
+export interface MarkerDto {
+  id: number,
+  name: string,
+  type: string,
+  spaceId: number
 }
