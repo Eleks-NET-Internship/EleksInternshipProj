eaddad5201c386d76aec8f44f78e01c00673577e
Nazarii Liushniak <nazarii.liushniak.pz.2022@lpnu.ua>
2025-06-20 19:00:00 +0300

    NAV-58-cleanup code

--- eleksinternshipproj.client/src/app/feature/calendar/components/calendar/calendar.component.ts
+++ eleksinternshipproj.client/src/app/feature/calendar/components/calendar/calendar.component.ts
@@ -154,10 +154,4 @@ export class CalendarComponent implements OnInit {
 
     return fullDate.toISOString();
   }
-
-  // private toLocalISOString(date: Date): string {
-  //   const pad = (n: number) => String(n).padStart(2, '0');
-  //   return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T` +
-  //         `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
-  // }
 }
