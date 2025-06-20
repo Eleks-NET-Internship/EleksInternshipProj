ccbc90e94483777d9e6f495c67366e17d6883072
MykhailivVolodymyr <volodymyrmykhailiv2005@gmail.com>
2025-06-20 19:06:36 +0300

    Create Task-card template

--- eleksinternshipproj.client/src/app/feature/tasks/components/tasks/tasks.component.ts
+++ eleksinternshipproj.client/src/app/feature/tasks/components/tasks/tasks.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TasksService } from '../../services/tasks.service';
+import { TaskDto } from '../../models/tasks-models';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
 
   constructor(private tasksService: TasksService) { }
+   private spaceId: number = 1; 
+   tasks: TaskDto[] = [];
+
+
+  ngOnInit(): void {
+     this.tasksService.getAllBySpaceId(this.spaceId)
+          .pipe(map((response: { data: TaskDto[] }) => response.data))
+          .subscribe({
+            next: (tasks) => {
+              this.tasks = tasks;
+              console.log(tasks);
+            },
+            error: (err) => {
+              console.error('Помилка при завантаженні завданнь:', err);
+            }
+          });
+  }
+
 }
