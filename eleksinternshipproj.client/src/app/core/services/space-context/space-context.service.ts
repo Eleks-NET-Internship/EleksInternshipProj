4e61ff8e710a2c7b5ce4cc557abdd0720cfc4e65
NazarSlobodian <nazar.slobodian.pz.2022@lpnu.ua>
2025-06-21 20:08:29 +0300

    NAV-57: Microoptimisations

--- eleksinternshipproj.client/src/app/core/services/space-context/space-context.service.ts
+++ eleksinternshipproj.client/src/app/core/services/space-context/space-context.service.ts
@@ -1,39 +1,44 @@
 import { Injectable } from '@angular/core';
 import { SpaceDto } from '../../../feature/spaces/models/spaces-models';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpaceContextService {
+  private readonly SPACE_KEY = "selectedSpace";
 
-  constructor() { }
+  private spaceSubject = new BehaviorSubject<SpaceDto | null>(this.getSpaceFromStorage());
+  space$ = this.spaceSubject.asObservable();
+
+  spaceId$: Observable<number | null> = this.space$.pipe(
+    map((space: SpaceDto | null) => space ? space.id : null)
+  );
+
+  spaceName$: Observable<string | null> = this.space$.pipe(
+    map((space: SpaceDto | null) => space ? space.name : null)
+  )
 
   storeSpaceContext(space: SpaceDto): void {
-    sessionStorage.setItem('selectedSpace', JSON.stringify(space));
+    sessionStorage.setItem(this.SPACE_KEY, JSON.stringify(space));
+    this.spaceSubject.next(space);
   }
 
   clearSpaceContext(): void {
-    sessionStorage.removeItem('selectedSpace');
+    sessionStorage.removeItem(this.SPACE_KEY);
+    this.spaceSubject.next(null);
   }
 
   getSpaceId(): number | null {
-    const space = this.getSpace()
-    if (!space)
-      return null;
-    return space.id;
+    return this.spaceSubject.getValue()?.id ?? null;
   }
 
-  getSpaceName(): string| null {
-    const space = this.getSpace()
-    if (!space)
-      return null;
-    return space.name;
+  getSpaceName(): string | null {
+    return this.spaceSubject.getValue()?.name ?? null;
   }
 
-  private getSpace(): SpaceDto | null {
-    const spaceStr = sessionStorage.getItem('selectedSpace');
-    if (!spaceStr)
-      return null;
-    return JSON.parse(spaceStr);
+  private getSpaceFromStorage(): SpaceDto | null {
+    const spaceStr = sessionStorage.getItem(this.SPACE_KEY);
+    return spaceStr ? JSON.parse(spaceStr) : null;
   }
 }
--- eleksinternshipproj.client/src/app/core/services/tokens/token-actions.service.ts
+++ eleksinternshipproj.client/src/app/core/services/tokens/token-actions.service.ts
@@ -6,17 +6,23 @@ import { Injectable } from '@angular/core';
 export class TokenActionsService {
 
   private readonly TOKEN_KEY = 'access_token';
+  private token: string | null;
 
-  constructor() { }
+  constructor() {
+    this.token = sessionStorage.getItem(this.TOKEN_KEY);
+  }
+
   getToken() {
-    return sessionStorage.getItem(this.TOKEN_KEY);
+    return this.token;
   }
 
   setToken(token: string) {
     sessionStorage.setItem(this.TOKEN_KEY, token);
+    this.token = token;
   }
 
   removeToken() {
     sessionStorage.removeItem(this.TOKEN_KEY);
+    this.token = null;
   }
 }
