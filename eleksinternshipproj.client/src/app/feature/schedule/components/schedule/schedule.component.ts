55136e419c9bfa370f64e82defbc7cf8e36acb64
Vadum-cmd <vadum.treskot@gmail.com>
2025-06-22 15:30:48 +0300

    fixed

--- eleksinternshipproj.client/src/app/feature/schedule/components/schedule/schedule.component.ts
+++ eleksinternshipproj.client/src/app/feature/schedule/components/schedule/schedule.component.ts
@@ -3,6 +3,7 @@ import { TimetableDto } from '../../models/schedule-models'
 import { Router } from '@angular/router';
 import {ScheduleService} from '../../services/schedule.service';
 import {TimetableContextService} from '../../services/timetable-context-service';
+import {SpaceContextService} from '../../../../core/services/space-context/space-context.service';
 
 @Component({
   selector: 'app-schedule',
@@ -16,12 +17,20 @@ export class ScheduleComponent implements OnInit {
   constructor(
     private scheduleService: ScheduleService,
     private context: TimetableContextService,
+    private spaceContextService: SpaceContextService,
     private router: Router) {}
 
   scheduleData: { [key: string]: any[] } = {};
 
+  private spaceId: number | null = null;
+
   ngOnInit(): void {
-    this.scheduleService.getBySpace(7).subscribe({
+    this.spaceId = this.spaceContextService.getSpaceId();
+    if (!this.spaceId) {
+      console.log("No spaceId in events init");
+      return;
+    }
+    this.scheduleService.getBySpace(this.spaceId).subscribe({
       next: (data) => {
         this.timetable = data;
         this.prepareScheduleData();
